9fe38ccbff57b1b01b19201fd92a71118da37b5c
hongantran <hongan.tran@beme.vn>
2024-04-21 01:10:33 +0700

    fix remove banner header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,11 @@
 import Header from '@/components/feature/Header'
 import Footer from '@/components/feature/Footer'
-import BannerHeader from '@/components/feature/BannerHeader'
 import NavigationCategory from '@/components/feature/NavigationCategory'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <main className=' min-h-screen' >
-            {/* <BannerHeader /> */}
+      
             <Header />
             <NavigationCategory />
             {children}
