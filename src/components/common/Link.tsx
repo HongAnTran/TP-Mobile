945258d3d47d0647e9ab79193f50e48623278eb3
hongantran <hongan.tran@beme.vn>
2024-07-01 22:44:00 +0700

    fix type

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -2,10 +2,12 @@
 import { cn } from "@/lib/utils";
 import LinkRoot, { LinkProps } from "next/link";
 import React, { AnchorHTMLAttributes  } from 'react'
+type MyLinkProps = LinkProps & Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps>;
 
-export default function Link(props: AnchorHTMLAttributes<HTMLAnchorElement> &  LinkProps ) {
+export default function Link(props: MyLinkProps) {
   return (
     <LinkRoot
+  
       {...props}
       className={cn(" hover:text-blue-500" , props.className)}
       prefetch={false}
