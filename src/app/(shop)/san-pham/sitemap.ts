641238c876ee4b76185adf032828020993d862d3
hongantran <hongan.tran@beme.vn>
2024-07-13 23:55:31 +0700

    up

--- src/app/(shop)/san-pham/sitemap.ts
+++ src/app/(shop)/san-pham/sitemap.ts
@@ -1,21 +1,17 @@
+import routes from "@/routes"
 import ProductsServiceApi from "@/services/productService"
 import { MetadataRoute } from "next"
 
 const domain = process.env.DOMAIN || "https://tpmobile.com.vn"
 
-export async function generateSitemaps() {
-  // Fetch the total number of products and calculate the number of sitemaps needed
-  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]
-}
- 
 export default async function sitemap({
-  id,
+  slug,
 }: {
-  id: number
+  slug: string
 }): Promise<MetadataRoute.Sitemap> {
   const {products} = await ProductsServiceApi.getList()
   return products.map((product) => ({
-    url: `${domain}/product/${id}`,
+    url: `${domain}/${routes.products}/${product.slug}`,
     lastModified: product.updated_at || product.created_at,
   }))
 }
