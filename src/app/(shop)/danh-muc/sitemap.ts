5a2cd65d3a15c7be67d468d53e6a6095e4da0ead
hongantran <hongan.tran@beme.vn>
2024-08-05 13:33:06 +0700

    up

---
+++ src/app/(shop)/danh-muc/sitemap.ts
@@ -0,0 +1,25 @@
+import CategoryServiceApi from "@/services/categoryService"
+import ProductsServiceApi from "@/services/productService"
+import { MetadataRoute } from "next"
+
+
+export async function generateSitemaps() {
+  const datas  = await CategoryServiceApi.getList()
+
+
+  return datas.map(data => ({ id: data.slug }))
+}
+const domain = process.env.DOMAIN || "https://tpmobile.com.vn"
+
+export default async function sitemap({
+  id,
+}: {
+  id: number
+}): Promise<MetadataRoute.Sitemap> {
+  const datas  = await CategoryServiceApi.getList()
+  return datas.map((data) => ({
+    url: `${domain}/danh-muc/${data.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly"
+  }))
+}
