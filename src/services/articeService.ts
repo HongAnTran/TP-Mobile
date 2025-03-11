91eb652666f0f1634f5f380968a7cdd23cffdfe2
HongAnTran <tranhongankrn.2001@gmail.com>
2025-03-11 23:45:07 +0700

    chore

--- src/services/articeService.ts
+++ src/services/articeService.ts
@@ -13,7 +13,7 @@ class ArticeService {
         tags: [this.url],
         revalidate: 60 * 60 * 24 * 30,
       },
-      isLogger: true,
+      // isLogger: true,
     });
   }
   async getDetail(slug: string) {
