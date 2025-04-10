7688c6f513f1deefe6544aa0aadde51bc7f5979b
HongAnTran <tranhongankrn.2001@gmail.com>
2025-04-10 22:26:50 +0700

    gg tag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Script from 'next/script';
-
+import { GoogleTagManager } from '@next/third-parties/google'
 const GoogleAnalytics = () => {
   return (
     <>
@@ -21,17 +21,7 @@ const GoogleAnalytics = () => {
               });
           `}
       </Script>
-
-      <Script id='google-tag-manager' strategy='afterInteractive'>
-        {`
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5B3VRRDF');</script>
-`}
-      </Script>
-     
+    <GoogleTagManager gtmId='GTM-5B3VRRDF'/>
     </>
   );
 };
