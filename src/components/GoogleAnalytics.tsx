7acc8b7a7e136759b9870eb96614bda896b1fc11
HongAnTran <tranhongankrn.2001@gmail.com>
2025-04-10 22:14:40 +0700

    fix

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -22,7 +22,7 @@ const GoogleAnalytics = () => {
           `}
       </Script>
 
-      <Script >
+      <Script id='google-tag-manager' strategy='afterInteractive'>
         {`
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
